96a62185eaf2c26d155fe09d657712700fb38857
Galiyga <galiyaga@yandex.ru>
2023-10-11 22:20:08 +0300

    условие, запрещающее вывод ответа от сервера

--- 5/script.js
+++ 5/script.js
@@ -1,5 +1,6 @@
 const socket = new WebSocket('wss://echo-ws-service.herokuapp.com/');
 const chat = document.querySelector('.chat');
+let hideNextMessage = false
 
 
 document.querySelector('.sendButton').addEventListener('click', () => {
@@ -13,29 +14,25 @@ document.querySelector('.geoButton').addEventListener('click', () => {
     navigator.geolocation.getCurrentPosition(function(position) {
         const location = `Latitude: ${position.coords.latitude}, Longitude: ${position.coords.longitude}`;
         socket.send(location);
-        displayMessage(`<a href="https://www.openstreetmap.org/#map=18/${position.coords.latitude}/${position.coords.longitude}">${location}</a>`, false);
+        displayMessage(`<a href="https://www.openstreetmap.org/#map=18/${position.coords.latitude}/${position.coords.longitude}">${location}</a>`, true);
     });
+
+    hideNextMessage = true
 });
 
-socket.addEventListener('message', (event) => {
-    displayMessage(event.data, false);
+socket.addEventListener('message', (message) => {
+    if (hideNextMessage) {
+        hideNextMessage = false
+    } else {
+        displayMessage(message.data, false);
+    }
 });
 
 
 function displayMessage(message, isClientMessage) {
+    console.log('displayMessage');
     const messageElement = document.createElement('div');
     messageElement.className = isClientMessage ? 'client-message message' : 'server-message message';
     messageElement.innerHTML = `<p>${message}</p>`;
     chat.appendChild(messageElement);
 }
-
-// function displayLocation(message, isClientMessage) {
-//     const messageElement = document.createElement('div');
-//     if(isClientMessage === false) {
-//         return
-//     } else {
-//         messageElement.className = 'client-message message';
-//         messageElement.innerHTML = `<p>${message}</p>`;
-//         chat.appendChild(messageElement);
-//     }
-// }
